6629a8d81bec7efdbad23c15f81cc250f9ad7db2
MaherUsman004 <mianaliusman004@gmail.com>
2024-01-24 18:12:56 +0500

    first commit

---
+++ functions programs ch9 C++/program 8 that satisfy equation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int Equation(int w, int x, int y, int z){
+    if(w*w+x*x+y*y==z*z)    return 0;
+    else return -1;
+}
+
+int main(){
+    int a,b,c,d;
+    cout<<"Enter value of a: ";
+    cin>>a;
+    cout<<"Enter value of b: ";
+    cin>>b;
+    cout<<"Enter value of c: ";
+    cin>>c;
+    cout<<"Enter value of d: ";
+    cin>>d;
+    Equation(a,b,c,d);
+}
